c0f0738fb06a001e279c2b2d0e72d36a45d2d344
Renat Shabanov <multizloy@gmail.com>
2023-09-08 16:54:21 +0300

    added edit and delete buttons in dashboard

--- puddle/puddle/puddle/urls.py
+++ puddle/puddle/puddle/urls.py
@@ -8,7 +8,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # сканирует все пути в приложении итемс
     path("items/", include("item.urls")),
     # будет проверять все ссылки в приложении core
     path("", include("core.urls")),
+    # сканирует все пути в приложении дашборд
+    path("dashboard/", include("dashboard.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
