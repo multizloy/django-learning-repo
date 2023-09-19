6720c06483f4fc2731949fce1754259631903cfb
Renat Shabanov <multizloy@gmail.com>
2023-09-19 18:20:09 +0300

    creating student-management app

---
+++ student-management-system/students/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Student
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, "students/index.html", {"students": Student.objects.all()})
+
+
+def view_student(request, id):
+    student = Student.objects.get(pk=id)
+    return HttpResponseRedirect(reverse("index"))
