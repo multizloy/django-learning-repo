fda7fd498d761f1e070b3c7fab077b5add2fc2a2
Renat Shabanov <multizloy@gmail.com>
2023-10-18 18:07:27 +0300

    employee app

--- LearnDjango/employee/urls.py
+++ LearnDjango/employee/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = "employees"
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path("create/", views.create, name="create"),
+    path("create/createData/", views.createData, name="create-data"),
 ]
--- LearnDjango/employee/views.py
+++ LearnDjango/employee/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
+from .models import Employee
+from django.urls import reverse
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, "employee/index.html", {})
+    myEmployees = Employee.objects.all().values()
+    context = {
+        "myEmployees": myEmployees,
+    }
+    return render(request, "employee/index.html", context)
+
+
+def create(request):
+    return render(request, "employee/createPage.html")
+
+
+def createData(request):
+    data_1 = request.POST["name"]
+    data_2 = request.POST["title"]
+    new_Employee = Employee(name=data_1, title=data_2)
+    new_Employee.save()
+    return redirect(reverse("employees:index"))
