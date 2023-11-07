fd30e30db7382a3fbbee7aeda6144ca457429835
Renat Shabanov <multizloy@gmail.com>
2023-11-07 17:22:53 +0300

    some changes in booba project

--- Booba/leads/models.py
+++ Booba/leads/models.py
@@ -26,12 +26,20 @@ class Lead(models.Model):
     age = models.IntegerField(default=0)
     organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     agent = models.ForeignKey("Agent", null=True, blank=True, on_delete=models.SET_NULL)
-    category = models.ForeignKey("Category", related_name="leads", null=True, blank=True, on_delete=models.SET_NULL)
-    description = models.TextField()
+    category = models.ForeignKey(
+        "Category",
+        related_name="leads",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+    description = models.TextField(null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
     phone_number = models.CharField(max_length=20)
     email = models.EmailField()
-    profile_picture = models.ImageField(null=True, blank=True, upload_to="profile_pictures/")
+    profile_picture = models.ImageField(
+        null=True, blank=True, upload_to="profile_pictures/"
+    )
     converted_date = models.DateTimeField(null=True, blank=True)
 
     objects = LeadManager()
--- Booba/leads/views.py
+++ Booba/leads/views.py
@@ -9,7 +9,7 @@ from .forms import (
     LeadModelForm,
     CustomUserCreationForm,
     AssignAgentForm,
-    CategoryModelForm,
+    # CategoryModelForm,
 )
 import django.views.generic as generic
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -239,35 +239,35 @@ class CategoryDetailView(LoginRequiredMixin, generic.DetailView):
         return queryset
 
 
-class CategoryCreateView(OrganisorAndLoginRequiredMixin, generic.CreateView):
-    template_name = "leads/category_create.html"
-    form_class = CategoryModelForm
+# class CategoryCreateView(OrganisorAndLoginRequiredMixin, generic.CreateView):
+#     template_name = "leads/category_create.html"
+#     form_class = CategoryModelForm
 
-    def get_success_url(self):
-        return reverse("leads:category-list")
+#     def get_success_url(self):
+#         return reverse("leads:category-list")
 
-    def form_valid(self, form):
-        category = form.save(commit=False)
-        category.organisation = self.request.user.userprofile
-        category.save()
-        return super(CategoryCreateView, self).form_valid(form)
+#     def form_valid(self, form):
+#         category = form.save(commit=False)
+#         category.organisation = self.request.user.userprofile
+#         category.save()
+#         return super(CategoryCreateView, self).form_valid(form)
 
 
-class CategoryUpdateView(OrganisorAndLoginRequiredMixin, generic.UpdateView):
-    template_name = "leads/category_update.html"
-    form_class = CategoryModelForm
+# class CategoryUpdateView(OrganisorAndLoginRequiredMixin, generic.UpdateView):
+#     template_name = "leads/category_update.html"
+#     form_class = CategoryModelForm
 
-    def get_success_url(self):
-        return reverse("leads:category-list")
+#     def get_success_url(self):
+#         return reverse("leads:category-list")
 
-    def get_queryset(self):
-        user = self.request.user
-        # initial queryset of leads for the entire organisation
-        if user.is_organisor:
-            queryset = Category.objects.filter(organisation=user.userprofile)
-        else:
-            queryset = Category.objects.filter(organisation=user.agent.organisation)
-        return queryset
+#     def get_queryset(self):
+#         user = self.request.user
+#         # initial queryset of leads for the entire organisation
+#         if user.is_organisor:
+#             queryset = Category.objects.filter(organisation=user.userprofile)
+#         else:
+#             queryset = Category.objects.filter(organisation=user.agent.organisation)
+#         return queryset
 
 
 class CategoryDeleteView(OrganisorAndLoginRequiredMixin, generic.DeleteView):
