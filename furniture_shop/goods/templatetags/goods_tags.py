fa635bc9385d05c2847c423748d553f306241787
Renat Shabanov <multizloy@gmail.com>
2024-01-31 20:50:35 +0300

    фильтрация + пагинация с сохраненением

--- furniture_shop/goods/templatetags/goods_tags.py
+++ furniture_shop/goods/templatetags/goods_tags.py
@@ -1,5 +1,7 @@
+from urllib.parse import urlencode
 from django import template
 from goods.models import Categories
+
 
 register = template.Library()
 
@@ -7,3 +9,9 @@ register = template.Library()
 @register.simple_tag
 def tag_categories():
     return Categories.objects.all()
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context["request"].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
