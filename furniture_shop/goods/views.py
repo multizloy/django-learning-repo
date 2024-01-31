038216f7a91f9dde243fb76a6db0ee9f9549e7bf
Renat Shabanov <multizloy@gmail.com>
2024-01-31 20:17:52 +0300

    pagination

--- furniture_shop/goods/views.py
+++ furniture_shop/goods/views.py
@@ -1,19 +1,24 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.core.paginator import Paginator
 from goods.models import Products
 
 # Create your views here.
 
 
 def catalog(request, category_slug):
+    page = request.GET.get("page", 1)
+    
     if category_slug == "vse-tovary":
         goods = Products.objects.all()
     else:
-        goods = get_object_or_404(Products.objects.filter(category__slug=category_slug))
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(page)
     context = {
         "title": "Home - Каталог",
-        "goods": goods,
+        "goods": current_page,
+        "slug_url": category_slug,
     }
     return render(request, "goods/catalog.html", context)
 
