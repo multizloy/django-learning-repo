a8ac4e507c3d866a567431f5e58dd157f0cf2e29
Renat Shabanov <multizloy@gmail.com>
2024-02-01 20:14:57 +0300

    сделали полную поисковую функцию

--- furniture_shop/shop/settings.py
+++ furniture_shop/shop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.postgres",
     # apps
     "main",
     "goods",
@@ -81,8 +82,12 @@ WSGI_APPLICATION = "shop.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "furniture-shop",
+        "USER": "root",
+        "PASSWORD": "root",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
