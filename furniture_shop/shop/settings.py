2e378eb8b41c446628ee21d9dbcccb02c01a5576
Renat Shabanov <multizloy@gmail.com>
2024-02-03 20:10:10 +0300

    login ready

--- furniture_shop/shop/settings.py
+++ furniture_shop/shop/settings.py
@@ -141,3 +141,5 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 INTERNAL_IPS = ["127.0.0.1"]
+
+AUTH_USER_MODEL = "users.User"
--- furniture_shop/users/views.py
+++ furniture_shop/users/views.py
@@ -1,10 +1,28 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib import auth
+from django.urls import reverse
+from .forms import User_Login_Form
 
 
 # Create your views here.
 def login(request):
+    if request.method == "POST":
+        form = User_Login_Form(data=request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                # return render(request, "users/profile.html")
+                return HttpResponseRedirect(reverse("main:index"))
+    else:
+        form = User_Login_Form()
+
     context = {
         "title": "Login page",
+        "form": form,
     }
     return render(request, "users/login.html", context)
 
@@ -28,4 +46,3 @@ def logout(request):
         "title": "Logout page",
     }
     return render(request, "users/logout.html", context)
-
