9a33820edff95e86e4e5078ba6c41b72604ee561
Renat Shabanov <multizloy@gmail.com>
2024-02-05 20:55:14 +0300

    удалить корзину до аякса

--- furniture_shop/cart/urls.py
+++ furniture_shop/cart/urls.py
@@ -6,6 +6,6 @@ app_name = "cart"
 
 urlpatterns = [
     path("cart_add/<slug:product_slug>/", cart_add, name="cart_add"),
-    path("cart_remove/<slug:product_slug>/", cart_remove, name="cart_remove"),
+    path("cart_remove/<int:cart_id>/", cart_remove, name="cart_remove"),
     path("cart_change/<slug:product_slug>/", cart_change, name="cart_change"),
 ]
